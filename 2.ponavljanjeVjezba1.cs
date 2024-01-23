dc3350e7a53d654e8db7bc7f1a598f06d3759b5d
matijaopacak06 <153612293+matijaopacak06@users.noreply.github.com>
2024-01-23 12:25:45 +0100

    Add files via upload

---
+++ 2.ponavljanjeVjezba1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.ponavljanjeVjezba1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            string rez = "";
+           if (s1.Length < 5)
+            {
+                rez = s1;
+            }
+           else
+            {
+               rez = s1.Substring(2, s1.Length - 4);
+
+            }
+           Console.WriteLine(rez);
+            Console.ReadKey();
+        }
+    }
+}
